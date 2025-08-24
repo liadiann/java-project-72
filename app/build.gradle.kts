9653ca0eeb1a0aea4da9899d4fa9c67a4304d9d6
Julia Kon <kon1976y@gmail.com>
2025-08-24 15:41:48 +0300

    add App.java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     checkstyle
-    java
+    application
     jacoco
     id("org.sonarqube") version "6.2.0.5505"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
@@ -13,8 +14,14 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.7.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 tasks.test {
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+    private static int getPort() {
+        var port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        var app = getApp();
+        app.start(getPort());
+    }
+}
